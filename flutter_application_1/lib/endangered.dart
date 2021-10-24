81d725638c9208b8dee9e6d7fb3bc88777b7e778
JustinZ765 <justinzhao2002@gmail.com>
2021-10-23 21:02:58 -0400

    Update endangered.dart

--- flutter_application_1/lib/endangered.dart
+++ flutter_application_1/lib/endangered.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/*class Endangered extends StatefulWidget {
-  const Endangered({Key? key}) : super(key: key);
-
-  @override
-  _Endangered createState() => _Endangered();
-}
-
-class _Endangered extends State<Endangered> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: entries.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          height: 50,
-          child: Center(child: Text('Entry ${entries[index]}')),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
-  }
-}*/
-
 class Endangered extends StatefulWidget {
   const Endangered({Key? key}) : super(key: key);
 
